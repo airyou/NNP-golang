19781ea335c3c99b9342265cc4a51b69dfe3f83f
you <greenair710@gmail.com>
2016-11-24 14:20:46 +0900

    CLEAN format by go format

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println(GetLast([]int{1,2,3}))
-	fmt.Println(GetLastButOne([]int{1,2,3}))
+func main() {
+	fmt.Println(GetLast([]int{1, 2, 3}))
+	fmt.Println(GetLastButOne([]int{1, 2, 3}))
 }
--- nnp2.go
+++ nnp2.go
@@ -2,12 +2,12 @@ package main
 
 import "errors"
 
-func GetLastButOne(list []int) (int, error){
-	if(list == nil){
+func GetLastButOne(list []int) (int, error) {
+	if list == nil {
 		return 0, errors.New("nilError")
 	}
 	l := len(list)
-	if(l < 2){
+	if l < 2 {
 		return 0, errors.New("EmptyError")
 	}
 	return list[l-2], nil
